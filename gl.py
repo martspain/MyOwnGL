ff4d9ddf3c3f5b237e6ab5222b9421a4b262963d
Martín España <46570760+martspain@users.noreply.github.com>
2021-07-26 23:03:57 -0600

    Polygon Fill - Initial
    First attempt on filling a polygon using line scan algorithm

--- gl.py
+++ gl.py
@@ -65,7 +65,7 @@ class Renderer(object):
         if (0 <= x < self.width) and (0 <= y < self.height):
             self.pixels[int(x)][int(y)] = color or self.curr_color
 
-    # Changes the color of 1 pixel of the viewport
+    # Changes the color of 1 pixel of the viewport (normalized)
     def glVertex(self, x, y, color = None):
         # Check coordinates to be between -1 and 1
         if x < -1 or x > 1:
@@ -126,6 +126,63 @@ class Renderer(object):
                 y += 1 if y0 < y1 else -1
                 limit += 1
 
+    # Gets the points used for the line
+    def glLinePoints(self, v0, v1):
+        x0 = v0.x
+        x1 = v1.x
+        y0 = v0.y
+        y1 = v1.y
+
+        # Avoids drawing a line from one position to itself
+        if x0 == x1 and y0 == y1:
+            return [V2(x0,y0)]
+
+        # Gets the differentials in absolute value so that the slope is always positive
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        # If slope is too steep then the axis are switched
+        steep = dy > dx
+
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        # Bresenham's Algorithm
+        offset = 0
+        limit = 0.5
+        m = dy/dx
+        y = y0
+
+        preanswer = []
+        answer = []
+        ycount = []
+
+        for x in range(x0, x1 + 1):
+            if steep:
+                preanswer.append(V2(y, x))
+            else:
+                preanswer.append(V2(x, y))
+
+            offset += m
+            if offset >= limit:
+                y += 1 if y0 < y1 else -1
+                limit += 1
+        
+        for ind in range(len(preanswer)):
+            if preanswer[ind].y not in ycount:
+                ycount.append(preanswer[ind].y)
+                answer.append(preanswer[ind])
+            
+        return answer
+
     # Loades an .obj model on screen
     def glLoadModel(self, filename, translate = V2(0.0,0.0),scale = V2(1.0,1.0), prev = False):
         model = Obj(filename)
@@ -150,6 +207,110 @@ class Renderer(object):
                 else:
                     self.glLine(V2(x0, y0), V2(x1, y1))
                 # self.glPoint(x0, y0)
+
+    def glPoly(self, poly, color = None):
+        for p in range(len(poly)):
+            nextIndex = p + 1
+            if nextIndex < len(poly):
+                x0 = poly[p].x
+                y0 = poly[p].y
+                x1 = poly[nextIndex].x
+                y1 = poly[nextIndex].y
+
+                self.glLine(V2(x0, y0), V2(x1, y1), color)
+            
+            else:
+                x0 = poly[p].x
+                y0 = poly[p].y
+                initx = poly[0].x
+                inity = poly[0].y
+                self.glLine(V2(x0, y0), V2(initx, inity), color)
+
+
+
+    # Filled poly
+    def glSolidPoly(self, poly, color = None):
+        #Verifies there is at least 3 vertexes
+        if len(poly) < 3:
+            if len(poly) == 1:
+                self.glPoint(int(poly[0].x), int(poly[0].y), color)
+            elif len(poly) == 2:
+                self.glLine(V2(poly[0].x, poly[0].y), V2(poly[1].x, poly[1].y), color)
+            else:
+                return
+        
+        xverts = []
+        yverts = []
+
+        linesdata = []
+
+        xmin = 0
+        xmax = 0
+        ymin = 0
+        ymax = 0
+
+        for v in range(len(poly)):
+            # Se guardan las coordenadas de x y y por separado
+            xverts.append(poly[v].x)
+            yverts.append(poly[v].y)
+
+            # Se dibuja el contorno del poligono
+            nextIndex = v + 1
+            if nextIndex < len(poly):
+                x0 = poly[v].x
+                y0 = poly[v].y
+                x1 = poly[nextIndex].x
+                y1 = poly[nextIndex].y
+
+                self.glLine(V2(x0, y0), V2(x1, y1), color)
+
+                linesdata.append(self.glLinePoints(V2(x0, y0), V2(x1, y1)))
+            
+            else:
+                x0 = poly[v].x
+                y0 = poly[v].y
+                initx = poly[0].x
+                inity = poly[0].y
+                self.glLine(V2(x0, y0), V2(initx, inity), color)
+                linesdata.append(self.glLinePoints(V2(x0, y0), V2(initx, inity)))
+
+        # Se encuentra el minimo y maximo de x y y
+        xmin = min(xverts)
+        xmax = max(xverts)
+        ymin = min(yverts)
+        ymax = max(yverts)
+
+        print(xmin, xmax, ymin, ymax)
+        
+        ycount = ymin
+        xcount = xmin
+        intercount = 0
+
+        while ycount < ymax:
+            while xcount < xmax:
+                for line in linesdata:
+                    for point in line:
+                        if point.x == xcount and point.y == ycount:
+                            intercount += 1
+                if intercount > 0:
+                    if (intercount%2) != 0:
+                        self.glPoint(xcount, ycount, color)
+                        xcount += 1
+                    else:
+                        xcount += 1
+                elif intercount == 0:
+                    xcount += 1
+            ycount += 1
+            xcount = xmin
+            intercount = 0
+                                
+        
+        
+        
+
+    
+    def glGetColor(self, pixel):
+        print(self.pixels[int(pixel.x)][int(pixel.y)])
 
     def glFinish(self, filename):
         with open(filename, "wb") as file:
--- tester.py
+++ tester.py
@@ -3,8 +3,21 @@ from gl import Renderer, V2, color
 import random
 
 #Se establece el ancho y la altura de la imagen
-width = 720
-height = 1080
+width = 900
+height = 450
+
+polyOne = [V2(165, 380), V2(185, 360), V2(180, 330), V2(207, 345), V2(233, 330), V2(230, 360), V2(250, 380), V2(220, 385), V2(205, 410), V2(193, 383)]
+
+polyTwo = [V2(321, 335), V2(288, 286), V2(339, 251), V2(374, 302)]
+
+polyThree = [V2(377, 249), V2(411, 197), V2(436, 249)]
+
+polyFour = [V2(413, 177), V2(448, 159), V2(502, 88), V2(553, 53), V2(535, 36), V2(676, 37), V2(660, 52),
+V2(750, 145), V2(761, 179), V2(672, 192), V2(659, 214), V2(615, 214), V2(632, 230), V2(580, 230),
+V2(597, 215), V2(552, 214), V2(517, 144), V2(466, 180)]
+
+polyFive = [V2(682, 175), V2(708, 120), V2(735, 148), V2(739, 170)]
+
 
 #Se crea la instancia
 rend = Renderer(width, height)
@@ -13,7 +26,36 @@ rend.glClearColor(0,0,0)
 rend.glColor(1,1,1)
 rend.glClear()
 
-rend.glLoadModel("statue.obj", V2(width/2, 0), V2(5.3, 5.3))
+rend.glSolidPoly(polyOne, color(1,0,0))
+rend.glSolidPoly(polyTwo, color(0,1,0))
+rend.glSolidPoly(polyThree, color(0,0,1))
+rend.glSolidPoly(polyFour, color(1,0,0))
+rend.glSolidPoly(polyFive, color(0,0,0))
+
+# for i in rend.glLinePoints(V2(165, 380), V2(185, 360)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(185, 360), V2(180, 330)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(180, 330), V2(207, 345)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(207, 345), V2(233, 330)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(233, 330), V2(230, 360)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(230, 360), V2(250, 380)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(250, 380), V2(220, 385)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(220, 385), V2(205, 410)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(205, 410), V2(193, 383)):
+#     rend.glPoint(i.x, i.y)
+# for i in rend.glLinePoints(V2(193, 383), V2(165, 380)):
+#     rend.glPoint(i.x, i.y)
+
+#rend.glGetColor(V2(682, 175))
+
+#rend.glLoadModel("statue.obj", V2(width/2, 0), V2(5.3, 5.3))
 # Diseño alterno
 #rend.glLoadModel("statue.obj", V2(1.5 * width/5, 0), V2(5.3, 5.3))
 #rend.glLoadModel("statue.obj", V2(3.5 * width/5, 0), V2(5.3, 5.3), True)
